2c7003dd5d59b51704ad10a3023e911d8ffabba5
mahdi falahati <falahati.mahdi@yahoo.com>
2023-05-05 09:05:02 +0330

    add: state management login

---
+++ src/store/auth/authActions.js
@@ -0,0 +1,63 @@
+import axios from 'axios'
+import { createAsyncThunk } from '@reduxjs/toolkit'
+
+const backendURL = 'http://37.32.27.143:8001/'
+
+
+export const userLogin = createAsyncThunk(
+    'auth/login',
+    async ({ email, password }, { rejectWithValue }) => {
+      try {
+        // configure header's Content-Type as JSON
+        const config = {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+        const { data } = await axios.post(
+          `${backendURL}/api/auth/users/login`,
+          {username : email, password },
+          config
+        )
+        // store user's token in local storage
+        localStorage.setItem('userToken', data.userToken)
+        return data
+      } catch (error) {
+        // return custom error message from API if any
+        if (error.response && error.response.data.message) {
+          return rejectWithValue(error.response.data.message)
+        } else {
+          return rejectWithValue(error.message)
+        }
+      }
+    }
+  )
+
+
+
+export const registerUser = createAsyncThunk(
+  'auth/register',
+  async ({ firstName, email, password }, { rejectWithValue }) => {
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+      await axios.post(
+        `${backendURL}/api/auth/users/register`,
+        { firstName, email, password },
+        config
+      )
+    } catch (error) {
+    // return custom error message from backend if present
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message)
+      } else {
+        return rejectWithValue(error.message)
+      }
+    }
+  }
+)
+
+
---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,51 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { registerUser, userLogin } from "./authActions";
+
+const userToken = localStorage.getItem("userToken")
+  ? localStorage.getItem("userToken")
+  : null;
+
+const initialState = {
+  loading: false,
+  userInfo: {}, // for user object
+  userToken: null, // for storing the JWT
+  error: null,
+  success: false, // for monitoring the registration process.
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers:(builder) => {
+    builder
+      .addCase(userLogin.pending, (state, { payload }) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(userLogin.fulfilled, (state, { payload }) => {
+        state.loading = false;
+        state.userInfo = payload;
+        state.userToken = payload.userToken;
+      })
+      .addCase(userLogin.rejected, (state, { payload }) => {
+        state.loading = false;
+        state.error = payload;
+      })
+      .addCase(registerUser.pending, (state, { payload }) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(registerUser.fulfilled, (state, { payload }) => {
+        state.loading = false;
+        state.success = true; // registration successful
+      })
+      .addCase(registerUser.rejected, (state, { payload }) => {
+        state.loading = false;
+        state.error = payload;
+      })
+ 
+  },
+});
+
+export default authSlice.reducer;
