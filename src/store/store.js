60c097d11ec1fd4f9d3bccca2452bc87a62108f2
mahdi falahati <falahati.mahdi@yahoo.com>
2023-05-30 22:36:44 +0330

    add form slice store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './auth/authSlice'
+import formSlice from "./network/formSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    networForm: formSlice,
   },
 
 })
