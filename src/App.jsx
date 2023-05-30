d361fc6efd33c3291015bed4130d41d3e4dc31a2
mahdi falahati <falahati.mahdi@yahoo.com>
2023-05-30 22:36:05 +0330

    add dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomeScreens from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
+import Dashboard from "./layout/Dashboard";
+import './assets/fonts/Sansation-Bold.ttf';
 
 function App() {
   return (
@@ -8,6 +10,10 @@ function App() {
       <Switch>
         <Route path="/" component={HomeScreens} exact />
         <Route path="/login" component={LoginScreen} exact />
+        <Route path="/dashboard" component={Dashboard} exact >
+            
+        </Route>
+
       </Switch>
     </Router>
   );
