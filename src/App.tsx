9fbb9f5b91f88e1ba1fa3ac52c80e5d2b584b671
Amir Soltani <amirsoltanik78@gmail.com>
2023-07-23 08:47:25 +0330

    fix: detail issue

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
           Component={selectElement(auth, pages.NetworksPage)}>
           <Route path="create" Component={pages.NetworkCreatePage} />
           <Route path=":networkId" Component={pages.NetworkEmptyPage}>
-            <Route path="detail" Component={pages.NetworkDetailPage}>
+            <Route path="" Component={pages.NetworkDetailPage}>
               <Route path="history" Component={pages.NetworkHistoryPage} />
             </Route>
           </Route>
