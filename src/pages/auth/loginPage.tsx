8f24c5d29789322284428ee49420620a087a2884
Amir Soltani <amirsoltanik78@gmail.com>
2023-07-23 08:19:46 +0330

    feat: login issue

--- src/pages/auth/loginPage.tsx
+++ src/pages/auth/loginPage.tsx
@@ -25,31 +25,33 @@ const LoginPage = () => {
       <div className="flex h-screen flex-col justify-between bg-[url('~/assets/images/loginBackground.png')] pl-10 pt-10">
         <div className="flex flex-row items-center">
           <img src={logo} className="h-24 w-24" />
-          <h1 className="ml-4 text-4xl font-bold text-white">Ariotech</h1>
+          <h1 className="ml-4 font-s text-4xl font-bold text-white">
+            Ariotech
+          </h1>
         </div>
 
         <div className="self-end md:w-2/3">
-          <h2 className="mb-4 ml-4 text-3xl text-white">
+          <h2 className="mb-4 ml-4 text-3xl font-bold text-white ">
             Intelligent Fiber Monitoring System
           </h2>
           <div className="mb-32 bg-[#D9D9D933]">
             <Form className="flex w-fit max-w-full flex-col pb-2.5 pl-7 pt-16">
               <div className="flex flex-row flex-wrap justify-between xl:flex-nowrap">
-                <span className="mt-1.5 w-48 text-xl font-normal text-white">
+                <span className="mt-1.5 w-72 text-xl font-normal text-white">
                   Username or email
                 </span>
                 <InputFormik
                   name="username"
-                  className="!h-12 w-96 rounded-lg bg-[#D9D9D9] px-4 text-sm"
+                  className="!h-11 w-80 rounded-lg bg-[#D9D9D9] px-4 text-sm"
                 />
               </div>
               <div className="flex flex-row flex-wrap items-start justify-between xl:flex-nowrap">
-                <span className="mt-1.5 w-48 text-xl font-normal text-white">
+                <span className="mt-1.5 w-72 text-xl font-normal text-white">
                   Password
                 </span>
                 <InputFormik
                   name="password"
-                  className="!h-12 w-96  rounded-lg bg-[#D9D9D9] px-4"
+                  className="!h-11 w-80  rounded-lg bg-[#D9D9D9] px-4"
                   type="password"
                 />
               </div>
