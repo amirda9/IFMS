f3284c4b63e071484b4867da6b31b2d583704ba9
Amirali Lotfi <liamirali.lotfi@gmail.com>
2023-08-17 11:34:09 +0330

    Add eye icon to inputs

--- src/container/formik/inputFormik.tsx
+++ src/container/formik/inputFormik.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useField} from 'formik';
 import {TextInput} from '~/components';
+import {IoEyeOutline} from 'react-icons/io5';
 
 type InputType = React.DetailedHTMLProps<
   React.InputHTMLAttributes<HTMLInputElement>,
@@ -9,21 +10,36 @@ type InputType = React.DetailedHTMLProps<
 type PropsType = Omit<InputType, 'value'> & {name: string};
 const InputFormik = ({name, className, ...props}: PropsType) => {
   const [fields, meta] = useField(name);
+
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const inputType =
+    props.type === 'password'
+      ? passwordVisible
+        ? 'password'
+        : 'text'
+      : props.type;
+
   return (
     <div
-      className={`flex flex-grow flex-col ${
+      className={`flex flex-grow flex-col items-start ${
         meta.touched && meta.error ? 'pb-1' : 'pb-5'
       }`}>
-      <TextInput
-        name={name}
-        className={`border border-solid ${
-          meta.error && meta.touched ? 'border-red-500' : ''
-        } ${className}`}
-        value={fields.value}
-        onChange={fields.onChange}
-        onBlur={fields.onBlur}
-        {...props}
-      />
+      <div className={`relative ${className}`}>
+        <TextInput
+          name={name}
+          className={`border border-solid ${
+            meta.error && meta.touched ? 'border-red-500' : ''
+          } ${className} w-full`}
+          value={fields.value}
+          onChange={fields.onChange}
+          onBlur={fields.onBlur}
+          type={inputType}
+          {...props}
+        />
+        <IoEyeOutline className="absolute top-2 right-2" />
+      </div>
+
       {meta.touched && meta.error ? (
         <label htmlFor={props.id} className="text-xs text-red-500">
           {meta.error}
--- src/pages/users/UserAuthenticationPage.tsx
+++ src/pages/users/UserAuthenticationPage.tsx
@@ -17,10 +17,14 @@ const UserAuthenticationPage: FC = () => {
         <Form className="flex h-full flex-col justify-between">
           <div className="flex flex-col">
             <Description label="New Password" items="start">
-              <InputFormik name="password" />
+              <InputFormik className="w-2/3" type="password" name="password" />
             </Description>
             <Description label="Confirm New Password" items="start">
-              <InputFormik name="passwordConfirmation" />
+              <InputFormik
+                className="w-2/3"
+                type="password"
+                name="passwordConfirmation"
+              />
             </Description>
           </div>
           <div className="flex flex-row gap-x-2 self-end">
