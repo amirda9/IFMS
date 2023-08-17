d8f2253f2983a697c6c80551feeed5c01d239948
Amirali Lotfi <liamirali.lotfi@gmail.com>
2023-08-17 11:57:06 +0330

    Fetch and show users list on users page sidebar

--- src/pages/users/UsersLayout.tsx
+++ src/pages/users/UsersLayout.tsx
@@ -1,17 +1,32 @@
 import {FC} from 'react';
 import {SidebarItem} from '~/components';
+import {useHttpRequest} from '~/hooks';
 import {SidebarLayout} from '~/layout';
 
 const UsersPage: FC = () => {
+  const userListQuery = useHttpRequest({
+    selector: state => state.http.userList,
+    initialRequests: (request, _state) => {
+      request('userList', undefined);
+    },
+  });
+
+  console.log(userListQuery);
+
   return (
     <SidebarLayout searchOnChange={() => {}} createTitle="Users">
-      {[...new Array(4)].map((_, value) => (
-        <SidebarItem
-          name={`User ${value + 1}`}
-          to={value.toString()}
-          key={value}
-        />
-      ))}
+      {userListQuery.state &&
+      userListQuery.state.httpRequestStatus === 'success' ? (
+        userListQuery.state.data!.map(user => (
+          <SidebarItem
+            name={user.username}
+            to={user.username}
+            key={user.username}
+          />
+        ))
+      ) : (
+        <p>loading</p>
+      )}
     </SidebarLayout>
   );
 };
