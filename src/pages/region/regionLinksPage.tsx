74c210e27307281feff74cdd147db95f0d63f975
Amir Soltani <amirsoltanik78@gmail.com>
2023-08-18 12:52:24 +0330

    feat: region link list

--- src/pages/region/regionLinksPage.tsx
+++ src/pages/region/regionLinksPage.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {SimpleBtn, Table} from '~/components';
+import {useHttpRequest} from '~/hooks';
+import {useParams} from 'react-router-dom';
 
 const columns = {
   index: {label: 'Index', size: 'w-[10%]'},
@@ -7,19 +9,34 @@ const columns = {
   source: {label: 'Source', size: 'w-[30%]'},
   destination: {label: 'Destination', size: 'w-[30%]'},
 };
-const dummy = [
-  {index: 1, name: 'link1', source: 'Station1', destination: 'Station1'},
-  {index: 2, name: 'link2', source: 'Station2', destination: 'Station2'},
-  {index: 3, name: 'link3', source: 'Station3', destination: 'Station3'},
-  {index: 4, name: 'link4', source: 'Station4', destination: 'Station4'},
-  {index: 5, name: 'link5', source: 'Station5', destination: 'Station5'},
-  {index: 6, name: 'link6', source: 'Station6', destination: 'Station6'},
-];
+
 const RegionLinksPage = () => {
+  const params = useParams<{regionId: string}>();
+  const {
+    state: {list},
+  } = useHttpRequest({
+    selector: state => ({list: state.http.regionLinkList}),
+    initialRequests: request => {
+      request('regionLinkList', {params: {region_id: params.regionId!}});
+    },
+  });
   return (
     <div className="flex h-full flex-col justify-between">
       <div className="h-5/6">
-        <Table cols={columns} items={dummy} width="w-3/5" />
+        <Table
+          cols={columns}
+          items={
+            list?.data?.map(link => ({
+              name: link.name,
+              source: '-',
+              destination: '-',
+            })) || []
+          }
+          dynamicColumns={['index']}
+          renderDynamicColumn={data => data.index}
+          width="w-3/5"
+          loading={list?.httpRequestStatus === 'loading'}
+        />
       </div>
       <div className="mr-4 flex flex-row gap-x-2 self-end">
         <SimpleBtn>Edit Links List</SimpleBtn>
