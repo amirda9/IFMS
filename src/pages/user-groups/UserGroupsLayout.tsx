c7c32ba3a08a81bed56bc84d209249c47a5725af
Amirali Lotfi <liamirali.lotfi@gmail.com>
2023-08-20 23:04:41 +0330

    Show error on user group API calls

--- src/pages/user-groups/UserGroupsLayout.tsx
+++ src/pages/user-groups/UserGroupsLayout.tsx
@@ -1,10 +1,11 @@
-import { FC } from "react"
-import { SidebarItem } from "~/components";
-import GeneralLoadingSpinner from "~/components/loading/GeneralLoadingSpinner";
-import { useHttpRequest } from "~/hooks";
-import { SidebarLayout } from "~/layout";
+import {FC, useEffect} from 'react';
+import {toast} from 'react-toastify';
+import {SidebarItem} from '~/components';
+import GeneralLoadingSpinner from '~/components/loading/GeneralLoadingSpinner';
+import {useHttpRequest} from '~/hooks';
+import {SidebarLayout} from '~/layout';
 
-const UserGroupsLayout:FC = () => {
+const UserGroupsLayout: FC = () => {
   const groupsListQuery = useHttpRequest({
     selector: state => state.http.groupList,
     initialRequests: (request, _state) => {
@@ -12,18 +13,30 @@ const UserGroupsLayout:FC = () => {
     },
   });
 
+  useEffect(() => {
+    if (groupsListQuery.state?.error) {
+      if (groupsListQuery.state?.error.status === 422) {
+      } else {
+        toast(groupsListQuery.state?.error.data.detail as string, {
+          type: 'error',
+        });
+      }
+    }
+  }, [groupsListQuery.state]);
+
   return (
     <SidebarLayout searchOnChange={() => {}} createTitle="Groups">
-      {groupsListQuery.state &&
-      groupsListQuery.state.httpRequestStatus === 'success' ? (
+      {groupsListQuery.state?.httpRequestStatus === 'success' ? (
         groupsListQuery.state.data!.map(group => (
           <SidebarItem name={group.name} to={group.id} key={group.id} />
         ))
-      ) : (
+      ) : groupsListQuery.state?.httpRequestStatus === 'loading' ? (
         <GeneralLoadingSpinner />
+      ) : (
+        <></>
       )}
     </SidebarLayout>
   );
-}
+};
 
-export default UserGroupsLayout
+export default UserGroupsLayout;
