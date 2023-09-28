1b2a7fb7e7c17264717daad41b09012d7185d7a1
Behnam maghsoudi <behnam.maghsodi1169@yahoo.com>
2023-09-28 22:13:46 +0330

    add some type to redux

--- src/store/slices/networkslice.ts
+++ src/store/slices/networkslice.ts
@@ -1,37 +1,47 @@
-import { createSlice} from "@reduxjs/toolkit";
+import {createSlice} from '@reduxjs/toolkit';
+
+type veiwerlists={
+    payload:string[];
+    type: string;
+}
 
 const initialState = {
-  type: "Cable",
-  networkviewers:[],
-  regionviewers:[],
-  stationviewers:[],
-  linkviewers:[],
+  type: 'Cable',
+  networkviewers: [],
+  regionviewers: [],
+  stationviewers: [],
+  linkviewers: [],
 } as any;
 
-const changetyperstate = createSlice({
-  name: "type",
 
+const changetyperstate = createSlice({
+  name: 'type',
   initialState,
   reducers: {
-   settypestate: (state, action) => {
+    settypestate: (state, action) => {
       state.type = action.payload;
     },
-    setnetworkviewers: (state, action) => {
+    setnetworkviewers: (state, action:veiwerlists) => {
       state.networkviewers = action.payload;
     },
-    setregionviewers: (state, action) => {
+    setregionviewers: (state, action:veiwerlists) => {
       state.regionviewers = action.payload;
     },
-
-  setstationviewers: (state, action) => {
-    state.stationviewers = action.payload;
+    setstationviewers: (state, action:veiwerlists) => {
+      state.stationviewers = action.payload;
+    },
+    setlinkviewers: (state, action:veiwerlists) => {
+      state.linkviewers = action.payload;
+    },
   },
-  setlinkviewers: (state, action) => {
-    state.linkviewers = action.payload;
-  },
-}
 });
 
-export const {settypestate,setnetworkviewers,setregionviewers,setstationviewers,setlinkviewers} = changetyperstate.actions;
+export const {
+  settypestate,
+  setnetworkviewers,
+  setregionviewers,
+  setstationviewers,
+  setlinkviewers,
+} = changetyperstate.actions;
 
 export default changetyperstate.reducer;
