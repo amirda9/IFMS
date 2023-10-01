aabf736089d5f49c2f115bcd486cdb0ae2f8b218
Behnam maghsoudi <behnam.maghsodi1169@yahoo.com>
2023-10-01 12:48:37 +0330

    fix:sourt region link list table

--- src/pages/region/regionLinksPage.tsx
+++ src/pages/region/regionLinksPage.tsx
@@ -4,7 +4,8 @@ import {useHttpRequest} from '~/hooks';
 import {useParams} from 'react-router-dom';
 import {useSelector} from 'react-redux';
 import {BASE_URL} from '~/constant';
-import { BsChevronDown } from 'react-icons/bs';
+import {BsChevronDown} from 'react-icons/bs';
+import {string} from 'yup';
 
 const columns = {
   index: {label: 'Index', size: 'w-[10%]'},
@@ -18,6 +19,13 @@ const RegionLinksPage = () => {
   const login = localStorage.getItem('login');
   const accesstoken = JSON.parse(login || '')?.data.access_token;
   const [userrole, setuserrole] = useState<any>('');
+  const [itemssorted, setItemssorted] = useState<
+    {
+      name: string;
+      source: string;
+      destination: string;
+    }[]
+  >([]);
   const getrole = async () => {
     const role = await fetch(`${BASE_URL}/auth/users/token/verify_token`, {
       headers: {
@@ -41,21 +49,48 @@ const RegionLinksPage = () => {
     },
   });
 
-  console.log(list?.data, 'list');
+  let items =
+    list?.data?.map(link => ({
+      name: link?.name,
+      source: link?.source,
+      destination: link?.destination,
+    })) || [];
+  
 
+
+  useEffect(() => {
+    setItemssorted(items.sort((a, b) => a.name.localeCompare(b.name, 'en-US')));
+  }, []);
+
+  const sortddata = (tabname: string, sortalfabet: boolean) => {
+    if (sortalfabet) {
+      items.sort(
+        (a: any, b: any) =>
+          -a[tabname.toLowerCase()].localeCompare(
+            b[tabname.toLowerCase()],
+            'en-US',
+          ),
+      );
+    } else {
+      items.sort((a: any, b: any) =>
+        a[tabname.toLowerCase()].localeCompare(
+          b[tabname.toLowerCase()],
+          'en-US',
+        ),
+      );
+    }
+    setItemssorted(items);
+  };
   return (
     <div className="flex h-full flex-col justify-between">
       <div className="relative h-5/6">
-      <BsChevronDown className='absolute  left-[22%] z-20  top-[8px]' />
         <Table
-          cols={columns}
-          items={
-            list?.data?.map(link => ({
-              name: link?.name,
-              source: link?.source,
-              destination: link?.destination,
-            })) || []
+          onclicktitle={(tabname: string, sortalfabet: boolean) =>
+            sortddata(tabname, sortalfabet)
           }
+          tabicon={'Name'}
+          cols={columns}
+          items={itemssorted}
           dynamicColumns={['index']}
           renderDynamicColumn={data => data.index}
           containerClassName="w-3/5"
