1f53c9d05cf9b3f08e2738ac47727017e783e151
Behnam maghsoudi <behnam.maghsodi1169@yahoo.com>
2023-10-11 13:58:57 +0330

    feat:chart
    -create 5 sample curveline
    -create some sample vertical line

--- src/components/chart/niva.tsx
+++ src/components/chart/niva.tsx
@@ -1,5 +1,5 @@
-
-import { ResponsiveLineCanvas } from "@nivo/line";
+import {useState} from 'react'
+import { ResponsiveLineCanvas,ResponsiveLine } from "@nivo/line";
 export default function MyResponsiveLineCanvas() {
 const data = [
 {
@@ -10,26 +10,61 @@ data: [
 { x: 3, y: 18 },
 { x: 4, y: 14 },
 { x: 5, y: 19 },
+{ x: 6, y: 22 },
+{ x: 7, y: 27 },
+{ x: 8, y: 29 },
+{ x: 10, y: 31 },
+{ x: 11, y: 35 },
 ],
 },
 ];
 
+
+
+const [xScale, setXScale] = useState<any>({
+ type: "point",
+ });
+ const [yScale, setYScale] = useState<any>({
+  type: "linear",
+ })  
+ const zoom = () => {
+  setXScale({
+  type: "point",
+  min: 2,
+  // max: 4,
+  });
+  setYScale({
+  type: "linear",
+  min: 15, 
+  // max: 20,
+  });
+  };
 return (
-<ResponsiveLineCanvas
+ <div className='cursor-pointer w-full h-full'>
+<ResponsiveLine
+
 data={data}
 margin={{ top: 50, right: 50, bottom: 50, left: 50 }}
-xScale={{ type: "point", }}
+xScale={xScale}
+yScale={yScale}
 xFormat="d"
-yScale={{ type: "linear" }}
 curve="linear"
 colors={{ scheme: "category10" }}
 lineWidth={3}
+useMesh={false}
 
-enablePoints={false}
+
+enablePoints={true}
 pointSize={10}
 pointColor={{ theme: "background" }}
 pointBorderWidth={2}
 pointBorderColor={{ from: "serieColor" }}
 />
+<button onClick={zoom} className='absoloute z-50 top-[200px] reight-[200px]'>Zoom</button>
+</div>
 );
 }
+
+
+
+
--- src/pages/configurations/optical-routes/chart.tsx
+++ src/pages/configurations/optical-routes/chart.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {GoZoomIn, GoZoomOut} from 'react-icons/go';
-
+import {ResponsiveLineCanvas, ResponsiveLine} from '@nivo/line';
 import Resultdata from '~/components/chart/rightbar';
 import arrowupchart from '~/assets/icons/arrowupchart.png';
 import hand from '~/assets/icons/hand.png';
@@ -14,71 +14,20 @@ import Min from '~/assets/icons/Min.png';
 import Avg from '~/assets/icons/Avg.png';
 import {SimpleBtn, Table} from '~/components';
 import {Line} from '@nivo/line';
-import { MyResponsiveLineCanvas } from '~/components/chart';
+import {MyResponsiveLineCanvas} from '~/components/chart';
 type chatrtabtype = {
   name: string;
   src: string;
 };
-const colorss = [
-  "#ffc107",
-  "#e7399c",
-  "#607d8b",
-  "#14d0f1",
-  "#ffab91",
-  "#6c5207",
-  "#1e4505",
-  "#86BBD8",
-  "#d6ee06",
-  "#8b008b",
-  "#906ed7",
-  "#d49416",
-  "#90A4AE",
-  "#3E2723",
-  "#FF6E40",
-  "#FFFF00",
-  "#5de00a",
-  "#e8e776",
-]
-
-// ######################################################
-const dataa = [
-  {
-    id: 'سری A',
-    data: [
-      {x: '2023-01-01', y: 12},
-      {x: '2023-01-02', y: 17},
-      {x: '2023-01-03', y: 18},
-      {x: '2023-01-04', y: 14},
-      {x: '2023-01-05', y: 19},
-    ],
-  },
-  {
-    id: 'سری B',
-    data: [
-      {x: '2023-01-01', y: 20},
-      {x: '2023-01-02', y: 16},
-      {x: '2023-01-03', y: 15},
-      {x: '2023-01-04', y: 21},
-      {x: '2023-01-05', y: 22},
-    ],
-  },
-  {
-    id: 'سری C',
-    data: [
-      {x: '2023-01-01', y: 10},
-      {x: '2023-01-02', y: 11},
-      {x: '2023-01-03', y: 13},
-      {x: '2023-01-04', y: 9},
-      {x: '2023-01-05', y: 12},
-    ],
-  },
-];
-
-// ######################################################
 type Verticalbotton = {
   name: string;
   classname: string;
+  onClick: () => void;
 };
+// ######################################################
+
+// ######################################################
+
 
 const columns = {
   index: {label: 'Index', size: 'w-[7%]'},
@@ -90,14 +39,225 @@ const columns = {
   Cumulative: {label: 'Cumulative Loss (dB)', size: 'w-[13%]'},
 };
 
+const allcurve = [
+  {
+    id: 'Cur',
+    data: [
+      {x: 1, y: 14},
+      {x: 2, y: 19},
+      {x: 3, y: 4},
+      {x: 4, y: 15},
+      {x: 5, y: 25},
+      {x: 6, y: 22},
+      {x: 7, y: 18},
+      {x: 8, y: 4},
+      {x: 10, y: 35},
+      {x: 11, y: 25},
+    ],
+  },
+  {
+    id: 'Ref',
+    data: [
+      {x: 1, y: 12},
+      {x: 2, y: 17},
+      {x: 3, y: 18},
+      {x: 4, y: 14},
+      {x: 5, y: 19},
+      {x: 6, y: 22},
+      {x: 7, y: 27},
+      {x: 8, y: 29},
+      {x: 10, y: 31},
+      {x: 11, y: 35},
+    ],
+  },
+  {
+    id: 'Max',
+    data: [
+      {x: 1, y: 8},
+      {x: 2, y: 12},
+      {x: 3, y: 14},
+      {x: 4, y: 28},
+      {x: 5, y: 2},
+      {x: 6, y: 5},
+      {x: 7, y: 9},
+      {x: 8, y: 18},
+      {x: 10, y: 2},
+      {x: 11, y: 14},
+    ],
+  },
+  {
+    id: 'Min',
+    data: [
+      {x: 1, y: 6},
+      {x: 2, y: 8},
+      {x: 3, y: 28},
+      {x: 4, y: 5},
+      {x: 5, y: 16},
+      {x: 6, y: 7},
+      {x: 7, y: 15},
+      {x: 8, y: 2},
+      {x: 10, y: 18},
+      {x: 11, y: 9},
+    ],
+  },
+  {
+    id: 'Avg',
+    data: [
+      {x: 1, y: 12},
+      {x: 2, y: 2},
+      {x: 3, y: 14},
+      {x: 4, y: 15},
+      {x: 5, y: 8},
+      {x: 6, y: 19},
+      {x: 7, y: 22},
+      {x: 8, y: 2},
+      {x: 10, y: 7},
+      {x: 11, y: 18},
+    ],
+  },
+];
+
+// -----------main --------------main ---------------- main ------------------- main --------------
 function Chart() {
   const [leftbartabselected, setLeftbartabselected] = useState('');
+  const [leftverticaltab, setLeftverticaltab] = useState<string>('');
   const [allchart, setAllchart] = useState<string[]>([]);
+  const [mousecursor, setMousecursor] = useState(false);
+  const [allcurveline, setAllcurveline] = useState<any>([]);
+  const [mousePosition, setMousePosition] = React.useState({x: 0, y: 0});
+  const [verticalLines, setVerticalLines] = useState<{x: number; y: number}[]>(
+    [],
+  );
 
+
+  
+  const [xScale, setXScale] = useState<any>({
+    type: 'point',tickFormat: (value:any) => Math.abs(Math.round(value / 5) * 5)
+  });
+  const [yScale, setYScale] = useState<any>({
+    type: 'linear'
+  });
+// ---- func ------func --------------- func ---------------- func ------------- func --------
+  const Events = () => {
+    setLeftverticaltab('Events');
+    const dataa = [
+      {x: 4, y: 15},
+      {x: 2, y: 19},
+    ];
+    setVerticalLines(prev => [...prev, ...dataa]);
+  };
+
+  const Trace = () => {
+    setLeftverticaltab('Trace');
+  };
+
+  const Measure = () => {
+    setLeftverticaltab('Measure');
+  };
+
+  const LinkView = () => {
+    setLeftverticaltab('LinkView');
+  };
+
+  const zoom = () => {
+    setXScale({
+      type: 'point',
+      min: 2,
+      // max: 4,
+    });
+    setYScale({
+      type: 'linear',
+      min: 15,
+      // max: 20,
+    });
+  };
+
+  const getColorForId = (id: string) => {
+    if (id === 'Cur') return '#273746';
+    if (id === 'Ref') return '#229954';
+    if (id === 'Max') return '#A93226';
+    if (id === 'Min') return '#2471A3';
+    return '#D4AC0D';
+  };
+
+  const showcurveline = (name: string) => {
+    const find = allcurve.findIndex((data: any) => data.id == name);
+    const find2 = allcurveline.findIndex((data: any) => data.id == name);
+
+    const find3 = allchart.findIndex((data: any) => data == name);
+    if (find2 > -1) {
+      const filtercurvs = allcurveline.filter((data: any) => data.id != name);
+      setAllcurveline(filtercurvs);
+    } else {
+      setAllcurveline((prev: any) => [...prev, allcurve[find]]);
+    }
+
+    if (find3 > -1) {
+      const filtercurvs = allchart.filter((data: any) => data != name);
+      setAllchart(filtercurvs);
+    } else {
+      setAllchart(prev => [...prev, name]);
+    }
+  };
+
+  const VerticalLine = ({xScale, yScale}: any) => {
+    const elements: any = [];
+    verticalLines.forEach((point,index) => {
+      const X = xScale(point.x);
+      const Y = yScale(point.y);
+      elements.push(
+        <line key={index} x1={X} y1={Y + 70} x2={X} y2={Y - 70} stroke="red" />,
+      );
+      elements.push(
+        <text
+          key={index}
+          x={X}
+          y={Y + 84}
+          fill="red"
+          fontSize={12}
+          textAnchor="middle">
+          2
+        </text>,
+      );
+    });
+    return elements;
+  };
+
+// تعریف یک تابع برای دریافت مختصات موس
+const handlePointMouseEnter = (event
+  :any, point:any) => {
+  // نمایش مختصات موس و نقطه در کنسول
+  console.log("Mouse coordinates:", event);
+  console.log("Point coordinates:", point);
+  };
+
+  React.useEffect(() => {
+    const updateMousePosition = (ev: any) => {
+      setMousePosition({x: ev.clientX, y: ev.pageY});
+    };
+    window.addEventListener('mousemove', updateMousePosition);
+    return () => {
+      window.removeEventListener('mousemove', updateMousePosition);
+    };
+  }, []);
+  const markers = [
+    {
+      axis: 'x',
+      value: 5,
+      lineStyle: {stroke: 'black', strokeWidth: 2},
+      legend: 'vertical line at x = 5',
+      legendOrientation: 'vertical',
+      legendPosition: 'top-left',
+      legendOffsetY: -5,
+    },
+  ];
+
+
+// ------ component --------- component ------------ component --------------- component ------------------
   const Chatrtabtype = ({name, src, ...props}: chatrtabtype) => {
     return (
       <button
-        onClick={() => setAllchart(prev => [...prev, name])}
+        onClick={() => showcurveline(name)}
         className={`${
           allchart.indexOf(name) > -1 ? 'bg-[#C6DFF8]' : 'bg-none'
         } mb-[2px] flex h-[40px] w-[87px]  flex-row items-center justify-between px-[4px]`}>
@@ -108,13 +268,12 @@ function Chart() {
       </button>
     );
   };
-
-  const Verticalbotton = ({name, classname}: Verticalbotton) => {
+  const Verticalbotton = ({name, classname, ...props}: Verticalbotton) => {
     return (
       <button
-        onClick={() => setLeftbartabselected(name)}
+        {...props}
         className={`${
-          leftbartabselected == name
+          leftverticaltab == name
             ? 'bg-[#006BBC]  text-[#ffff] '
             : 'bg-[#C6DFF8] text-[#000000]'
         } mt-[1px]  box-border w-[40px] text-[18px] font-light leading-[21.87px] ${classname}`}>
@@ -122,6 +281,7 @@ function Chart() {
       </button>
     );
   };
+  // ##############################################################################################
   return (
     <div className="box-border flex w-full flex-col overflow-x-hidden p-[10px]">
       <div className="flex h-[540px] w-full flex-row">
@@ -129,29 +289,36 @@ function Chart() {
         <div className="flex h-full w-[87px] flex-col">
           <div className="flex h-[360px] w-full flex-row">
             <div className="flex w-[40px] flex-col">
-              <Verticalbotton name="Trace" classname="pb-[5px] h-[72px]" />
-              <Verticalbotton name="Events" classname="pb-[15px] h-[79px]" />
-              <Verticalbotton name="Measure" classname="pb-[27px] h-[79px]" />
-              <button
-                onClick={() => setLeftbartabselected('Link View')}
-                className={`${
-                  leftbartabselected == 'Link View'
-                    ? 'bg-[#006BBC]  text-[#ffff] '
-                    : 'bg-[#C6DFF8] text-[#000000]'
-                } mt-[1px] h-[94px] w-[40px] text-[18px] font-light leading-[21.87px]`}>
-                <>
-                  <div className="mt-[-5px] rotate-90">Link</div>
-                  <div className="mt-[20px] rotate-90">View</div>
-                </>
-              </button>
+              <Verticalbotton
+                onClick={() => Trace()}
+                name="Trace"
+                classname="pb-[5px] h-[72px]"
+              />
+              <Verticalbotton
+                onClick={() => Events()}
+                name="Events"
+                classname="pb-[15px] h-[79px]"
+              />
+              <Verticalbotton
+                onClick={() => Measure()}
+                name="Measure"
+                classname="pb-[27px] h-[79px]"
+              />
+              <Verticalbotton
+                onClick={() => LinkView()}
+                name={`Link${''}View`}
+                classname="pb-[35px] h-[99px]"
+              />
             </div>
 
             <div className="ml-[15px] flex w-[30px] flex-col">
               <img
+                onClick={() => setMousecursor(false)}
                 src={arrowupchart}
                 className="h-[30px] w-[30px] cursor-pointer"
               />
               <img
+                onClick={() => setMousecursor(true)}
                 src={hand}
                 className="mt-[20px] h-[40px] w-[30px] cursor-pointer"
               />
@@ -178,35 +345,58 @@ function Chart() {
           <Chatrtabtype name="Min" src={Min} />
           <Chatrtabtype name="Avg" src={Avg} />
         </div>
-        {/* ---- center ------- center ------------------ center ------------- center ------------- center ------------ */}
+        {/* ---- chart ------- chart ------------------ chart ------------- chart ------------- chart ------------ */}
         <div className="mx-[10px] flex h-full w-[calc(100%-474px)] flex-col">
           <div className="h-[calc(100%-50px)] w-full">
-            <MyResponsiveLineCanvas />
-            {/* <svg width={700} height={500}>
-              <Line
-                data={dataa}
-                width={500}
-                height={300}
+            <div
+          
+              className={`${
+                mousecursor ? 'cursor-pointer' : 'cursor-default'
+              } h-full w-full`}>
+              <ResponsiveLine
+                data={allcurveline}
                 margin={{top: 50, right: 50, bottom: 50, left: 50}}
-                xScale={{type: 'time', format: '%Y-%m-%d'}}
-                xFormat="time:%Y-%m-%d"
-                yScale={{type: 'linear'}}
-                curve="monotoneX"
-                // colors={{scheme: 'category10'}}
-                colors={colorss}
+                xScale={xScale}
+                yScale={yScale}
+                colors={({id}) => getColorForId(id)}
+                xFormat="d"
+                curve="linear"
+                 onMouseEnter={(point:any, event:any)=>handlePointMouseEnter(point, event)} 
                 lineWidth={3}
+                // useMesh={true}
+              
+                layers={[
+                  'grid',
+                  // "markers",
+                  'axes',
+                  'areas',
+                  'crosshair',
+                  'lines',
+                  // "points",
+                  // 'slices',
+                  "mesh",
+                  'legends',
+                  VerticalLine, // اضافه کردن تابع VerticalLine به لایه ها
+                ]}
+                // markers={markers}
                 enablePoints={true}
                 pointSize={10}
                 pointColor={{theme: 'background'}}
                 pointBorderWidth={2}
                 pointBorderColor={{from: 'serieColor'}}
               />
-            </svg> */}
+              <button
+                onClick={zoom}
+                className="absoloute reight-[200px] top-[200px] z-50">
+                Zoom
+              </button>
+            </div>
           </div>
         </div>
         {/* ---- reightbar ------- reightbar ------------------ reightbar ------------- reightbar ------------- reightbar ------------ */}
         <Resultdata />
       </div>
+<div>{mousePosition.x}</div>
       {/* ---- tabel ------- tabel ------------------ tabel ------------- tabel ------------- tabel ------------ */}
       <Table
         onclicktitle={(tabname: string, sortalfabet: boolean) => () => {}}
@@ -223,6 +413,5 @@ function Chart() {
 }
 
 export default Chart;
-// {
-//   render: () => <LineCanvas {...commonProperties} />
-// }
+
+
