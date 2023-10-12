2d59443326735e4a6eeb0f79fc2992bab7568af3
Behnam maghsoudi <behnam.maghsodi1169@yahoo.com>
2023-10-12 16:14:49 +0330

    fix(region link list):change sort logic

--- src/pages/region/regionLinksPage.tsx
+++ src/pages/region/regionLinksPage.tsx
@@ -20,6 +20,7 @@ const RegionLinksPage = () => {
   const login = localStorage.getItem('login');
   const accesstoken = JSON.parse(login || '')?.data.access_token;
   const [userrole, setuserrole] = useState<any>('');
+  const [tabname, setTabname] = useState('Name');
   const [itemssorted, setItemssorted] = useState<
     {
       name: string;
@@ -27,6 +28,7 @@ const RegionLinksPage = () => {
       destination: string;
     }[]
   >([]);
+
   const getrole = async () => {
     const role = await fetch(`${BASE_URL}/auth/users/token/verify_token`, {
       headers: {
@@ -37,12 +39,15 @@ const RegionLinksPage = () => {
     }).then(res => res.json());
     setuserrole(role.role);
   };
+  
   useEffect(() => {
     getrole();
   }, []);
   const params = useParams<{regionId: string}>();
+
   const {
-    state: {list},request
+    state: {list},
+    request,
   } = useHttpRequest({
     selector: state => ({
       list: state.http.regionLinkList,
@@ -54,8 +59,8 @@ const RegionLinksPage = () => {
     },
     onUpdate: (lastState, state) => {
       if (
-        (lastState.add?.httpRequestStatus === 'loading' &&
-          state.add!.httpRequestStatus === 'success')
+        lastState.add?.httpRequestStatus === 'loading' &&
+        state.add!.httpRequestStatus === 'success'
       ) {
         request('regionLinkList', {params: {region_id: params.regionId!}});
       }
@@ -68,7 +73,6 @@ const RegionLinksPage = () => {
       source: link?.source,
       destination: link?.destination,
     })) || [];
-  
 
 
   useEffect(() => {
@@ -95,7 +99,7 @@ const RegionLinksPage = () => {
     setItemssorted(items);
   };
 
-  const save=()=>{
+  const save = () => {
     const appenddata = [];
     const removedata = [];
     const alllist = list?.data || [];
@@ -118,8 +122,7 @@ const RegionLinksPage = () => {
       }
     }
 
-
-    let first =list?.data || [];
+    let first = list?.data || [];
     if (first.length == 0 && newregionlinklist?.length == 0) {
     } else {
       request('addregionLinkList', {
@@ -131,21 +134,21 @@ const RegionLinksPage = () => {
         data: {links_id: removedata || []},
       });
     }
-  }
-
+  };
 
   return (
     <div className="flex h-full flex-col justify-between">
       <div className="relative h-5/6">
         <Table
-          onclicktitle={(tabname: string, sortalfabet: boolean) =>
-            sortddata(tabname, sortalfabet)
-          }
-          tabicon={'Name'}
+          tabicon={tabname}
+          onclicktitle={(tabname: string, sortalfabet: boolean) => {
+            setTabname(tabname);
+            sortddata(tabname, sortalfabet);
+          }}
           cols={columns}
           items={itemssorted}
           dynamicColumns={['index']}
-          renderDynamicColumn={data => data.index}
+          renderDynamicColumn={data => data.index + 1}
           containerClassName="w-3/5"
           loading={list?.httpRequestStatus === 'loading'}
         />
