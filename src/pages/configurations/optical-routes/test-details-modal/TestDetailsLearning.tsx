b168f060404f62c31c5df7cface74f55b610d921
Behnam maghsoudi <behnam.maghsodi1169@yahoo.com>
2023-10-22 22:03:03 +0330

    style:change test setup modal style

--- src/pages/configurations/optical-routes/test-details-modal/TestDetailsLearning.tsx
+++ src/pages/configurations/optical-routes/test-details-modal/TestDetailsLearning.tsx
@@ -1,9 +1,49 @@
-import {FC} from 'react';
+import {FC, useRef, useState} from 'react';
 import {Description, Select, TextInput} from '~/components';
-
+import dateicon from '~/assets/images/dateicon.png'
+import './index.css'
 const TestDetailsLearning: FC = () => {
+  const [selectedradio,setSelectedradio]=useState("On")
+  const [selectedradio2,setSelectedradio2]=useState("On")
+  const firstdateref:any=useRef(null)
+  const secenddateref:any=useRef(null)
+  function RadioButton({name}: any) {
+    return (
+      <div className="flex flex-row items-center">
+        <button
+          onClick={() => setSelectedradio(name)}
+          className="flex h-[20px] w-[20px] items-center justify-center rounded-[10px] bg-[#ffffff]">
+          <div
+            className={`h-[10px] w-[10px] rounded-[5px] ${
+              selectedradio == name ? 'bg-[#0E9836]' : 'bg-[#ffffff]'
+            } `}></div>
+        </button>
+        <span className="ml-[8px] text-[20px] font-light text-[#000000]">
+          {name}
+        </span>
+      </div>
+    );
+  }
+  function RadioButton2({name}: any) {
+    return (
+      <div className="flex flex-row items-center">
+        <button
+          onClick={() => setSelectedradio2(name)}
+          className="flex h-[20px] w-[20px] items-center justify-center rounded-[10px] bg-[#ffffff]">
+          <div
+            className={`h-[10px] w-[10px] rounded-[5px] ${
+              selectedradio2 == name ? 'bg-[#0E9836]' : 'bg-[#ffffff]'
+            } `}></div>
+        </button>
+        <span className="ml-[8px] text-[20px] font-light text-[#000000]">
+          {name}
+        </span>
+      </div>
+    );
+  }
   return (
     <div className="flex flex-col gap-y-8">
+    
       <Description labelClassName='basis-64' label="Targeted Count Per Cycle">
         <TextInput name="count-per-cycle" type="number" />
       </Description>
@@ -11,24 +51,12 @@ const TestDetailsLearning: FC = () => {
       <Description labelClassName='basis-64' label="Start a New Cycle">
         <div className="flex flex-col gap-y-4">
           <div className="flex flex-row items-center">
-            <input
-              name="start-cycle"
-              type="radio"
-              value="on"
-              defaultChecked={true}
-              className="mr-2"
-            />
-            <label>On</label>
-            <TextInput type="date" className="w-48 ml-6" />
+          <RadioButton  name="On"/>
+            <input ref={firstdateref} type="date" className="h-8 rounded-md border border-black px-2 w-48 ml-6" />
+            <img src={dateicon}  onClick={()=> firstdateref.current.showPicker()} className='w-[35px] h-[35px] ml-[5px]'   />
           </div>
           <div className="flex flex-row items-center gap-x-4">
-            <input
-              name="start-cycle"
-              type="radio"
-              value="every"
-              className="-mr-2"
-            />
-            <label>Every</label>
+          <RadioButton  name="Every"/>
             <TextInput defaultValue={2} className="w-16" />
             <Select className='w-26' >
               <option>Months</option>
@@ -41,24 +69,14 @@ const TestDetailsLearning: FC = () => {
       <Description labelClassName='basis-64' label={<span className='flex items-center gap-x-2'><span>Increase Target by</span> <TextInput className='w-16' /></span>}>
         <div className="flex flex-col gap-y-4">
           <div className="flex flex-row items-center">
-            <input
-              name="increase-target"
-              type="radio"
-              value="on"
-              defaultChecked={true}
-              className="mr-2"
-            />
-            <label>On</label>
-            <TextInput type="date" className="w-48 ml-6" />
+          <RadioButton2  name="On"/>
+        
+          <input ref={secenddateref} type="date" className="h-8 rounded-md border border-black px-2 w-48 ml-6" />
+            <img src={dateicon}  onClick={()=> secenddateref.current.showPicker()} className='w-[35px] h-[35px] ml-[5px]'   />
           </div>
           <div className="flex flex-row items-center gap-x-4">
-            <input
-              name="increase-target"
-              type="radio"
-              value="every"
-              className="-mr-2"
-            />
-            <label>Every</label>
+          <RadioButton2  name="Every"/>
+
             <TextInput defaultValue={2} className="w-16" />
             <Select className='w-26' >
               <option>Months</option>
--- src/pages/configurations/optical-routes/test-details-modal/TestDetailsTestProgram.tsx
+++ src/pages/configurations/optical-routes/test-details-modal/TestDetailsTestProgram.tsx
@@ -1,6 +1,6 @@
-import {FC} from 'react';
+import {FC, useRef, useState} from 'react';
 import {Description, TextInput} from '~/components';
-
+import dateicon from '~/assets/images/dateicon.png'
 const times = {
   Hourly: 'Hour(s)',
   Daily: 'Day(s)',
@@ -8,52 +8,73 @@ const times = {
   Yearly: 'Year(s)',
   Every: '',
 };
-
+ type RadioButton={
+  name:string
+ }
 const TestDetailsTestProgram: FC = () => {
+  const [selectedradio,setSelectedradio]=useState("On")
+  const [selectedradio2,setSelectedradio2]=useState("Hourly")
+  const firstdateref:any=useRef(null)
+  const secenddateref:any=useRef(null)
+  function RadioButton({name}: RadioButton) {
+    return (
+      <div className="flex flex-row items-center">
+        <button
+          onClick={() => setSelectedradio(name)}
+          className="flex h-[20px] w-[20px] items-center justify-center rounded-[10px] bg-[#ffffff]">
+          <div
+            className={`h-[10px] w-[10px] rounded-[5px] ${
+              selectedradio == name ? 'bg-[#0E9836]' : 'bg-[#ffffff]'
+            } `}></div>
+        </button>
+        <span className="ml-[8px] text-[20px] font-light text-[#000000]">
+          {name}
+        </span>
+      </div>
+    );
+  }
+  function RadioButton2({name}: RadioButton) {
+    return (
+      <div className="flex flex-row items-center">
+        <button
+          onClick={() => setSelectedradio2(name)}
+          className="flex h-[20px] w-[20px] items-center justify-center rounded-[10px] bg-[#ffffff]">
+          <div
+            className={`h-[10px] w-[10px] rounded-[5px] ${
+              selectedradio2 == name ? 'bg-[#0E9836]' : 'bg-[#ffffff]'
+            } `}></div>
+        </button>
+        <span className="ml-[8px] text-[20px] font-light text-[#000000]">
+          {name}
+        </span>
+      </div>
+    );
+  }
   return (
     <div className="flex flex-col gap-y-8">
       <Description label="Starting Date">
-        <div className="flex items-center gap-x-8">
-          <TextInput type="date" />
-          <TextInput type="time" />
-          <span>
-            <input type="radio" />
-            <label className="ml-2">As Soon As Possible</label>
-          </span>
+        <div className="flex items-center">
+        <input ref={firstdateref} type="date" className="h-8 rounded-md border border-black px-2 w-48 ml-6" />
+            <img src={dateicon}  onClick={()=> firstdateref.current.showPicker()} className='w-[35px] h-[35px] ml-[5px] mr-[25px]'   />
+          <TextInput className='mr-[30px]' type="time" />
+          <RadioButton  name="As Soon As Possible"/>
         </div>
       </Description>
-      <Description label="Starting Date">
-        <div className="flex items-center gap-x-8">
-          <TextInput type="date" />
-          <TextInput type="time" />
-          <span>
-            <input type="radio" />
-            <label className="ml-2">Indefinite</label>
-          </span>
+      <Description label="End Date">
+        <div className="flex items-center">
+        <input ref={secenddateref} type="date" className="h-8 rounded-md border border-black px-2 w-48 ml-6" />
+            <img src={dateicon}  onClick={()=> secenddateref.current.showPicker()} className='w-[35px] h-[35px] ml-[5px] mr-[25px]'   />
+          <TextInput className='mr-[30px]' type="time" />
+          <RadioButton  name="Indefinite"/>
         </div>
       </Description>
       <Description label="Periodicity">
         <div className="flex flex-col gap-y-4">
-          <label className="flex items-center">
-            <input type="radio" name="periodicity" defaultChecked />
-            <span className="ml-2">Hourly</span>
-          </label>
-          <label className="flex items-center">
-            <input type="radio" name="periodicity" />
-            <span className="ml-2">Daily</span>
-          </label>
-          <label className="flex items-center">
-            <input type="radio" name="periodicity" />
-            <span className="ml-2">Monthly</span>
-          </label>
-          <label className="flex items-center">
-            <input type="radio" name="periodicity" />
-            <span className="ml-2">Yearly</span>
-          </label>
-          <label className="flex items-center">
-            <input type="radio" name="periodicity" />
-            <span className="ml-2">Every</span>
-          </label>
+        <RadioButton2 name={'Hourly'} />
+          <RadioButton2 name={'Daily'} />
+          <RadioButton2 name={'Monthly'} />
+          <RadioButton2 name={'Yearly'} />
+          <RadioButton2 name={'Every'} />
         </div>
         <div className='ml-16'>
           <span>Every</span>
