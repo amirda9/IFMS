59ef64080202ec98fc40985abe1f11a0d598eb26
Behnam maghsoudi <behnam.maghsodi1169@yahoo.com>
2023-10-25 10:58:30 +0330

    fix:System Settings - System - Page View Modification

--- src/pages/configurations/system-settings/system/SystemPage.tsx
+++ src/pages/configurations/system-settings/system/SystemPage.tsx
@@ -1,79 +1,79 @@
 import {Description, Select} from '~/components';
 import SystemSettingsMain from '../SystemSettingsMain';
+import {ReactNode} from 'react';
+type Rowinputtype = {
+  name: string;
+  children: ReactNode;
+};
+
+const Rowinput = ({name, children}: Rowinputtype) => {
+  return (
+    <div className="flex w-[700px] flex-row justify-between mb-[6px]">
+      <span className="w-[400px] text-[20px] font-light leading-[24.2px] text-[#000000]">
+        {name}
+      </span>
+      <div className="flex w-[350px]">{children}</div>
+    </div>
+  );
+};
+// -------------------- main --------------------------- main ---------------------- main --------------------- main --------
 
 const SystemPage = () => {
   return (
     <SystemSettingsMain>
       <div className="flex flex-col gap-y-4">
-        <Description label="Break Strategy" labelClassName="w-80">
-          <Select className="w-1/5">
+        <Rowinput name="Break Strategy">
+        <Select  className="w-full">
             <option value="" className="hidden">
-              Select
+            Skip
             </option>
             <option value={undefined} className="hidden">
-              Select
+            Skip
             </option>
             <option>Continue</option>
             <option>Skip</option>
           </Select>
-        </Description>
-
-        <Description
-          label="Fiber Test Setup Definition Strategy"
-          labelClassName="w-80">
-          <Select className="w-1/5">
+        </Rowinput>
+        
+        <Rowinput name="Fiber Test Setup Definition Strategy">
+        <Select  className="w-full">
             <option value="" className="hidden">
-              Select
+            None
             </option>
             <option value={undefined} className="hidden">
-              Select
+            None
             </option>
             <option>None</option>
             <option>Monitoring Only</option>
             <option>Both</option>
           </Select>
-        </Description>
+        </Rowinput>
 
-        <Description label="Data Save Policy" labelClassName="w-80">
-          <Select className="w-1/5">
+        <Rowinput name="Data Save Policy">
+        <Select  className="w-full">
             <option value="" className="hidden">
-              Select
+            Data Save Policy
             </option>
             <option value={undefined} className="hidden">
-              Select
+            Data Save Policy
             </option>
-            <option>Save Trace File</option>
+            <option>Data Save Policy</option>
             <option>Don’t Save Trace File</option>
           </Select>
-        </Description>
+        </Rowinput>
 
-        <Description label="Test Type" labelClassName="w-80">
-          <Select className="w-1/5">
+        <Rowinput name="Test Type">
+        <Select  className="w-full">
             <option value="" className="hidden">
-              Select
+            Monitoring
             </option>
             <option value={undefined} className="hidden">
-              Select
+            Monitoring
             </option>
             <option>Monitoring</option>
-            <option>Proactive Maintenance</option>
+            <option>Maintenance</option>
           </Select>
-        </Description>
-
-        <Description
-          label="Degraded fiber Handling Strategy"
-          labelClassName="w-80">
-          <Select className="w-1/5">
-            <option value="" className="hidden">
-              Select
-            </option>
-            <option value={undefined} className="hidden">
-              Select
-            </option>
-            <option>Enabled</option>
-            <option>Disable</option>
-          </Select>
-        </Description>
+        </Rowinput>
       </div>
     </SystemSettingsMain>
   );
