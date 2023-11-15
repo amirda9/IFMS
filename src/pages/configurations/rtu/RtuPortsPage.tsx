c4c38665cff5a5262de06d9a5760c2a0a1750950
Behnam maghsoudi <behnam.maghsodi1169@yahoo.com>
2023-11-15 10:34:55 +0330

    fix:work on integration rtuportspage

--- src/pages/configurations/rtu/RtuPortsPage.tsx
+++ src/pages/configurations/rtu/RtuPortsPage.tsx
@@ -1,10 +1,15 @@
-import {FC} from 'react';
-import {SimpleBtn} from '~/components';
+import {FC, useEffect, useState} from 'react';
+import {Select, SimpleBtn} from '~/components';
 import RtuPortItem, {RtuItemProps} from './RtuPortItem';
-import { useHttpRequest } from '~/hooks';
-import { useParams } from 'react-router-dom';
-import { log } from 'console';
-
+import {useHttpRequest} from '~/hooks';
+import {useParams} from 'react-router-dom';
+import {log} from 'console';
+import {$GET, $POST, $PUT} from '~/util/requestapi';
+import {string} from 'yup';
+import classNames from '~/util/classNames';
+import {IoOpenOutline, IoTrashOutline} from 'react-icons/io5';
+import {ControlledSelect} from '~/components';
+import {BsPlusLg} from 'react-icons/bs';
 const ports: RtuItemProps[] = [
   {
     index: 1,
@@ -84,42 +89,232 @@ const ports: RtuItemProps[] = [
   },
 ];
 
+type Iprops = {
+  classname: string;
+  onclick: Function;
+};
+
+type allportstype = {
+  optical_route_id: string;
+  state: string;
+  index: number;
+  id: string;
+  new?: boolean;
+  end_station: {
+    id: string;
+    name: string;
+  };
+  optical_route: {
+    id: string;
+    name: string;
+  };
+  length: number;
+};
+
+type allupdatesportstype = {
+  index: number;
+  optical_route_id: string;
+  state: string;
+  id: string;
+};
+
+type allcreateportstype = {
+  index: number;
+  optical_route_id: string;
+  state: string;
+};
+
+type alldeletedports={
+  
+}
+const opticalRouteOptions = [
+  {label: 'Optical Route 1', id: '11111'},
+  {label: 'Optical Route 2', id: '22222'},
+  {label: 'Optical Route 3', id: '33333'},
+  {label: 'Optical Route 4', id: '44444'},
+];
+// ---- component ------ component -----------------component-------------------------------
+const Addbox = ({classname, onclick}: Iprops) => {
+  return (
+    <div
+      className={`flex flex-row items-center justify-between opacity-0 hover:opacity-100 ${classname}`}>
+      <button
+        onClick={() => onclick()}
+        className="mr-[3px] h-[30px] w-[30px] cursor-pointer items-center justify-center rounded-[15px] bg-[#32C65D]">
+        <BsPlusLg color="white" size={35} className="ml-[-2.5px] mt-[-2.5px]" />
+      </button>
+      <div className="w-full  border-t-[2px] border-dashed  border-[#32C65D]"></div>
+    </div>
+  );
+};
+// -------- main ------------------ main -------------------- main -------------main --------
 const RtuPortsPage: FC = () => {
   const params = useParams();
-  const {
-    state: {rtuports},
-    request,
-  } = useHttpRequest({
-    selector: state => ({
-      rtuports: state.http.rtuPorts,
-    }),
-    initialRequests: request => {
-      request('rtuPorts', {params: {rtu_id: params.rtuId || ''}});
-    },
-    // onUpdate: (lastState, state) => {
-    //   if (
-    //     lastState.update?.httpRequestStatus === 'loading' &&
-    //     state.update?.httpRequestStatus === 'success'
-    //   ) {
-    //     request('rtuDetail', {params: {rtu_Id: params.rtuId || ''}});
-    //   }
-    // },
-    // onUpdate: lastState => {
-    //   if (
-    //     lastState.create?.httpRequestStatus === 'loading' &&
-    //     create?.httpRequestStatus === 'success'
-    //   ) {
-    //     request('allLinks', undefined);
-    //     // navigate('../' + create?.data?.link_id);
-    //   }
-    // },
-  });
+  const [allrtuports, setAllrtuports] = useState<allportstype[]>([]);
+  const [allcreateports, setAllcreateports] = useState<allcreateportstype[]>([]);
+  const [allupdatesports, setAllupdatesports] = useState<allupdatesportstype[]>([]);
+  const [alldeletedports,setAlldeletedports]=useState([])
+  console.log(allrtuports, 'allrtuports');
+  console.log(allupdatesports, 'allupdatesports');
+  console.log(allcreateports, 'allcreatertu');
 
- console.log(rtuports,'rtuportsrtuportsrtuports');
- const allports:any=rtuports?.data || [];
+  // const {
+  //   state: {rtuports},
+  //   request,
+  // } = useHttpRequest({
+  //   selector: state => ({
+  //     rtuports: state.http.rtuPorts,
+  //   }),
+  //   initialRequests: request => {
+  //     request('rtuPorts', {params: {rtu_id: params.rtuId || ''}});
+  //   },
+  // });
+  const getrtuports = async () => {
+    const getdata = await $GET(`otdr/rtu/${params.rtuId}/ports`);
+    setAllrtuports(
+      getdata.map((data: allportstype, index: number) => ({...data, index})),
+    );
+    console.log(getdata, '');
+  };
+
+  useEffect(() => {
+    getrtuports();
+  }, []);
+  // console.log(rtuports, 'rtuportsrtuportsrtuports');
+
+  const save = async () => {
+    if (allcreateports && allcreateports?.length > 0) {
+      const sencreate = await $POST(
+        `otdr/rtu/${params.rtuId}/ports`,
+        allcreateports,
+      );
+    }
+    if (allrtuports && allrtuports?.length > 0) {
+      const sendupdates = await $PUT(
+        `otdr/rtu/${params.rtuId}/ports`,
+        allrtuports,
+      );
+    }
+    getrtuports();
+  };
+
+  const add = (x: number) => {
+    setAllcreateports(prev => [
+      ...prev,
+      {index: x, optical_route_id: '', state: ''},
+    ]);
+    setAllrtuports(prev => [
+      ...prev,
+      {
+        index: x,
+        optical_route_id: '',
+        state: '',
+        id: '',
+        new: true,
+        end_station: {
+          id: '',
+          name: '',
+        },
+        optical_route: {
+          id: '',
+          name: '',
+        },
+        length: 0,
+      },
+    ]);
+  };
+  
+
+  const changeupticalroute = (index: number, id: string, name: string) => {
+    //We first update the data that we have mapped on the page.
+    const oldallports = JSON.parse(JSON.stringify(allrtuports));
+    const findportIndex = oldallports.findIndex(
+      (data: any) => data.index == index,
+    );
+    oldallports[findportIndex].optical_route_id = id;
+    oldallports[findportIndex].optical_route.name = name;
+    oldallports[findportIndex].optical_route.id = id;
+    setAllrtuports(oldallports);
+    // ------------------------------------------------------------------
+    if (oldallports[findportIndex].new) {
+      //If this port was among the new ports, we would update this port in the list of new ports.
+      const oldcreatedports = JSON.parse(JSON.stringify(allcreateports));
+      const fincreateports = oldcreatedports.findIndex(
+        (data: any) => data.index == index,
+      );
+      oldcreatedports[fincreateports].optical_route_id = id;
+      setAllcreateports(oldcreatedports);
+    } else {
+      const oldupdates = JSON.parse(JSON.stringify(allupdatesports));
+      const find = oldupdates.findIndex((data: any) => data.index == index);
+      if (find > -1) {
+        //If this port was among the updated ports, we would update this port in the list of updated ports.
+        oldupdates[find].optical_route_id = id;
+        setAllupdatesports(oldupdates);
+      } else {
+        //else we add this port in the list of updated ports
+        setAllupdatesports(prev => [
+          ...prev,
+          {index: index, optical_route_id: id, state: '', id: ''},
+        ]);
+      }
+    }
+  };
+
+  const changestate = (index: number, state: string) => {
+    //We first update the data that we have mapped on the page.
+    const oldallports = JSON.parse(JSON.stringify(allrtuports));
+    const findportIndex = oldallports.findIndex(
+      (data: any) => data.index == index,
+    );
+    oldallports[findportIndex].state = state;
+    setAllrtuports(oldallports);
+    // ------------------------------------------------------------------
+    if (oldallports[findportIndex].new) {
+      //If this port was among the new ports, we would update this port in the list of new ports.
+      const oldcreatedports = JSON.parse(JSON.stringify(allcreateports));
+      const fincreateports = oldcreatedports.findIndex(
+        (data: any) => data.index == index,
+      );
+      oldcreatedports[fincreateports].state = state;
+      setAllcreateports(oldcreatedports);
+    } else {
+      const oldupdates = JSON.parse(JSON.stringify(allupdatesports));
+      const find = oldupdates.findIndex((data: any) => data.index == index);
+      if (find > -1) {
+        //If this port was among the updated ports, we would update this port in the list of updated ports.
+        oldupdates[find].state = state;
+        setAllupdatesports(oldupdates);
+      } else {
+        //else we add this port in the list of updated ports
+        setAllupdatesports(prev => [
+          ...prev,
+          {index: index, optical_route_id: '', state: state, id: ''},
+        ]);
+      }
+    }
+  };
+
+
+  const deleteports=(index:number,id:string)=>{
+    const oldallports = JSON.parse(JSON.stringify(allrtuports));
+    const findportIndex = oldallports.findIndex(
+      (data: any) => data.index == index,
+    );
+    if(oldallports[findportIndex].new){
+      const newallports=oldallports.filter((data:any) => data.index != index);
+      setAllrtuports(newallports)
+
+      const newcreatedport=allcreateports.filter(data => data.index != index)
+      setAllcreateports(newcreatedport)
+    }else{
+
+    }
+  }
+  // ####################################################################
   return (
     <div className="flex flex-col gap-y-8">
-      <div className="flex flex-grow flex-col gap-y-4">
+      <div className="flex min-h-[60vh] flex-grow flex-col gap-y-4">
         <div className="flex items-center px-4">
           <span className="basis-16">Index</span>
           <span className="basis-64">Optical Route</span>
@@ -128,12 +323,90 @@ const RtuPortsPage: FC = () => {
           <span className="basis-40">State</span>
           <span className="basis-40"></span>
         </div>
-        {allports.length>0 && allports.map((port:any, i:any) => (
-          <RtuPortItem key={i} {...port} />
-        ))}
+        {Array.isArray(allrtuports) &&
+          allrtuports.length > 0 &&
+          allrtuports?.map((data: allportstype, index: any) => (
+            <div
+              className={classNames(
+                'flex items-center rounded-lg p-4',
+                data.state == 'deactivate'
+                  ? 'bg-red-300'
+                  : data.state == 'activate'
+                  ? 'bg-success'
+                  : 'bg-[#E59D9D]',
+              )}>
+              <span className="basis-16">{index}</span>
+              <div className="basis-64 pr-[20px]">
+                <Select
+                  onChange={e =>
+                    changeupticalroute(
+                      index,
+                      e.target.value.split('_')[0],
+                      e.target.value.split('_')[1],
+                    )
+                  }
+                  className="h-10 w-full">
+                  <option value="" className="hidden">
+                  {data.optical_route_id == ''?'select':data.optical_route.name}
+                  </option>
+                  <option value={undefined} className="hidden">
+                  {data.optical_route_id == ''?'select':data.optical_route.name}
+                  </option>
+                  {opticalRouteOptions.map((data, index) => (
+                    <option
+                      value={`${data.id}_${data.label}`}
+                      key={index}
+                      className="text-[20px] font-light leading-[24.2px] text-[#000000]">
+                      {data.label}
+                    </option>
+                  ))}
+                </Select>
+              </div>
+              {data.optical_route_id.length > 0 ? (
+                <>
+                  <span className="basis-44 ">
+                    {data.state != 'deactivate' && data.end_station.name}
+                  </span>
+                  <span className="basis-32">
+                    {data.state != 'deactivate' && data.length}
+                  </span>
+                  <div className="basis-40">
+                    {data.state != 'deactivate' && (
+                      <ControlledSelect
+                        className="h-10 w-[80%]"
+                        value={data.optical_route.name}
+                        options={[
+                          {label: 'Activated', payload: {active: true}},
+                          {label: 'Deactivated', payload: {active: true}},
+                        ]}
+                        onChange={e =>
+                          changestate(index, e.toString().toLowerCase())
+                        }
+                        setValueProp={option => option.label}
+                      />
+                    )}
+                  </div>
+                  <div className="flex basis-40 flex-row justify-around gap-x-4">
+                    {data.state != 'deactivate' && (
+                      <>
+                        <IoOpenOutline size={30} />
+                        <IoTrashOutline onClick={()=>deleteports(index,data.id)} className="text-red-500 cursor-pointer" size={30} />
+                      </>
+                    )}
+                  </div>
+                </>
+              ) : null}
+            </div>
+          ))}
+        <Addbox
+          classname={'ml-[calc(5%-56px)]  h-[30px] xl:ml-[calc(6%-56px)]'}
+          onclick={() => add(allrtuports.length)}
+        />
       </div>
       <div className="flex gap-x-4 self-end">
-        <SimpleBtn>Save</SimpleBtn>
+        <SimpleBtn type="button" onClick={save}>
+          Save
+        </SimpleBtn>
         <SimpleBtn>Cancel</SimpleBtn>
       </div>
     </div>
--- src/util/requestapi.ts
+++ src/util/requestapi.ts
@@ -15,5 +15,27 @@ export const $GET =async(url:string)=>{
 
 
 
+export const $POST =async(url:string,data:any)=>{
+  return await fetch(`${BASE_URL}/${url}`, {
+   method: "post",
+   body:data,
+   headers: {
+     Authorization: `Bearer ${accesstoken}`,
+     Accept: 'application.json',
+     'Content-Type': 'application/json',
+   },
+  }).then(res => res.json());
+ } 
 
+ export const $PUT =async(url:string,data:any)=>{
+  return await fetch(`${BASE_URL}/${url}`, {
+   method: "post",
+   body:data,
+   headers: {
+     Authorization: `Bearer ${accesstoken}`,
+     Accept: 'application.json',
+     'Content-Type': 'application/json',
+   },
+  }).then(res => res.json());
+ } 
 
