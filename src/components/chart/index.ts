de30eaad4fccf69a4ce1bbae7501beb0aaf9d25a
Behnam maghsoudi <behnam.maghsodi1169@yahoo.com>
2023-12-24 10:24:24 +0330

    fix<conponentschartindex>:minor issues fix

--- src/components/chart/index.ts
+++ src/components/chart/index.ts
@@ -1,6 +1,6 @@
 export {default as Resultdata} from './result';
 export {default as Opticalroute} from './opticalroute';
 export {default as Alarms} from './alarms';
-export {default as Detailbox} from './detailbox';
+
 
 export {default as MyResponsiveLineCanvas} from './niva';
