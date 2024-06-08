94d9b5afbc1141a95858f9eb33f330cbfc906749
Behnam maghsoudi <behnam.maghsodi1169@yahoo.com>
2024-06-08 16:38:51 +0330

    fix<networksPage>:Removing redundant codes and optimizing codes

--- src/pages/network/networksPage.tsx
+++ src/pages/network/networksPage.tsx
@@ -1,38 +1,9 @@
-import  {FC} from 'react';
-import {useHttpRequest} from '~/hooks';
+import {FC} from 'react';
 import NetworktreeLayout from '~/layout/networktreeLayout';
-
 const NetworksPage: FC = () => {
-
-  const {
-    request,
-    state: {list},
-  } = useHttpRequest({
-    selector: state => ({
-      list: state.http.networkList,
-      deleteRequest: state.http.networkDelete,
-    }),
-    initialRequests: request => {
-      if (list?.httpRequestStatus !== 'success') {
-        request('networkList', undefined);
-      }
-    },
-    onUpdate: (lastState, state) => {
-      if (
-        lastState.deleteRequest?.httpRequestStatus === 'loading' &&
-        state.deleteRequest!.httpRequestStatus === 'success'
-      ) {
-        request('networkList', undefined);
-      }
-    },
-  });
-
   return (
     <NetworktreeLayout>
-        <div className='w-full flex flex-col mb-[-18px]'>
-      
-        </div>
-
+      <div className="mb-[-18px] flex w-full flex-col"></div>
     </NetworktreeLayout>
   );
 };
