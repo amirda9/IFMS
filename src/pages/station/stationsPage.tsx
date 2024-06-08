2c30681b7011c498659e79190c6500e88cfe6778
Behnam maghsoudi <behnam.maghsodi1169@yahoo.com>
2024-06-08 16:24:03 +0330

    refactor<station/stationsPage>:Removing redundant codes and optimizing codesگ

--- src/pages/station/stationsPage.tsx
+++ src/pages/station/stationsPage.tsx
@@ -1,65 +1,8 @@
-import React, {useEffect, useState} from 'react';
-import {SidebarItem} from '~/components';
-import {SidebarLayout} from '~/layout';
-import Cookies from 'js-cookie';
-import {networkExplored} from '~/constant';
-import {useHttpRequest} from '~/hooks';
-import {useSelector} from 'react-redux';
-import {BASE_URL} from './../../constant';
 import NetworktreeLayout from '~/layout/networktreeLayout';
 const StationsPage = () => {
-  const {stationDetail, networkDetail} = useSelector(
-    (state: any) => state.http,
-  );
-  const login = localStorage.getItem('login');
-  const accesstoken = JSON.parse(login || '')?.data.access_token;
-  const [userrole, setuserrole] = useState<any>('');
-  const getrole = async () => {
-    const role = await fetch(`${BASE_URL}/auth/users/token/verify_token`, {
-      headers: {
-        Authorization: `Bearer ${accesstoken}`,
-        Accept: 'application?.json',
-        'Content-Type': 'application/json',
-      },
-    }).then(res => res?.json());
-    setuserrole(role.role);
-    console.log(role, 'getrole');
-  };
-  useEffect(() => {
-    getrole();
-  }, []);
-  const networkId = Cookies.get(networkExplored);
-  const {
-    state: {stations, deleteRequest},
-    request,
-  } = useHttpRequest({
-    selector: state => ({
-      stations: state.http.allStations,
-      networkstations: state.http.networkstations,
-      deleteRequest: state.http.stationDelete,
-    }),
-    initialRequests: request => {
-      if (networkId) {
-        request('allStations', undefined);
-        request('networkstations', {params: {network_id: networkId}});
-      }
-    },
-    onUpdate: (lastState, state) => {
-      if (
-        lastState.deleteRequest?.httpRequestStatus === 'loading' &&
-        state.deleteRequest!.httpRequestStatus === 'success'
-      ) {
-        request('allStations', undefined);
-      }
-    },
-  });
-
-  // console.log(networkstations,'networkstations');
-
   return (
     <NetworktreeLayout>
-    <>
-    </>
+      <></>
     </NetworktreeLayout>
   );
 };
