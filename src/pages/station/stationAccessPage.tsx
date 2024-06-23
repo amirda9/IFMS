622f27a0c296aa6205980028809293f55363de9e
Behnam maghsoudi <behnam.maghsodi1169@yahoo.com>
2024-06-23 11:57:02 +0330

    fix<stationAccessPage:fix some bugs feat<stationAccessPage>:add error alert

--- src/pages/station/stationAccessPage.tsx
+++ src/pages/station/stationAccessPage.tsx
@@ -1,6 +1,7 @@
 import React, {useMemo, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {useParams} from 'react-router-dom';
+import {toast} from 'react-toastify';
 import {Select, SimpleBtn, Table} from '~/components';
 import {UserRole} from '~/constant/users';
 import {useAppSelector, useHttpRequest} from '~/hooks';
@@ -9,6 +10,7 @@ import {
   setstationviewersstatus,
 } from '~/store/slices/networkslice';
 import {AccessEnum} from '~/types';
+import {$Post, $Put} from '~/util/requestapi';
 
 const columns = {
   index: {label: 'Index', size: 'w-[10%]'},
@@ -16,9 +18,11 @@ const columns = {
   region: {label: 'Region', size: 'w-[30%]'},
   station: {label: 'Station', size: 'w-[30%]'},
 };
-type Iprops={
-  regionId:string,networkId:string,stationId:string
-  }
+type Iprops = {
+  regionId: string;
+  networkId: string;
+  stationId: string;
+};
 const StationAccessPage = () => {
   const {regionDetail, networkDetail} = useSelector((state: any) => state.http);
   const {regionidadmin, networkidadmin} = useSelector(
@@ -26,6 +30,7 @@ const StationAccessPage = () => {
   );
   const loggedInUser = useAppSelector(state => state.http.verifyToken?.data)!;
   const login = localStorage.getItem('login');
+  const [loading,setLoading]=useState(false)
   const [tabname, setTabname] = useState('User');
   const dispatch = useDispatch();
   const [itemssorted, setItemssorted] = useState<
@@ -56,41 +61,103 @@ const StationAccessPage = () => {
       });
       request('userList', undefined);
     },
-    onUpdate: (lastState, state) => {
-      if (
-        lastState.update?.httpRequestStatus === 'loading' &&
-        state.update!.httpRequestStatus === 'success'
-      ) {
+    // onUpdate: (lastState, state) => {
+    //   if (
+    //     lastState.update?.httpRequestStatus === 'loading' &&
+    //     state.update!.httpRequestStatus === 'success'
+    //   ) {
+    //     request('stationAccessList', {
+    //       params: {station_id: params.stationId!},
+    //     });
+    //   }
+    // },
+  });
+
+  const changeAdmin = async () => {
+    // const viewerWithoutAdmin =viewers?.data?.users
+    //     .filter(viewer => viewer.access !== AccessEnum.admin)
+    //     .map(viewer => ({
+    //       user_id: viewer.user.id,
+    //       access_types: AccessEnum.viewer,
+    //     })) || [];
+    try {
+      const admin = viewers?.data?.users.find(
+        viewer => viewer.access === AccessEnum.admin,
+      );
+      const updateadminresponse = await $Put(
+        `otdr/station/${params.stationId!}/access/admin`,
+        {user_id: userAdmin || admin!.user.id!},
+      );
+      if (updateadminresponse?.status == 201) {
+        toast('It was done successfully', {
+          type: 'success',
+          autoClose: 1000,
+        });
+      } else {
+        toast('Encountered an error', {type: 'error', autoClose: 1000});
+      }
+    } catch (error) {
+      console.log(`update admin error is :${error}`);
+    }
+
+    //   request('stationAddadmin', {
+    //     params: {station_id: params.stationId!},
+    //     data: {user_id: userAdmin || admin!.user.id!},
+    //   });
+  };
+
+  const saveAdmin = async () => {
+    try {
+      setLoading(true)
+      const viewerWithoutAdmin =
+        viewers?.data?.users
+          .filter(viewer => viewer.access !== AccessEnum.admin)
+          .map(viewer => ({
+            user_id: viewer.user.id,
+            access_types: AccessEnum.viewer,
+          })) || [];
+      const admin = viewers?.data?.users.find(
+        viewer => viewer.access === AccessEnum.admin,
+      );
+      viewerWithoutAdmin.push({
+        user_id: userAdmin || admin!.user.id,
+        access_types: AccessEnum.admin,
+      });
+      const [updateacsecc, updateadmin] = await Promise.all([
+        $Post(`otdr/station/${params?.stationId!}/access/viewers`, {
+          users: network?.stationviewers,
+        }),
+        $Put(`otdr/station/${params.stationId!}/access/admin`, {
+          user_id: userAdmin || admin!.user.id!,
+        }),
+      ]);
+      if (updateacsecc?.status == 200 && updateadmin?.status == 201) {
         request('stationAccessList', {
           params: {station_id: params.stationId!},
         });
+        toast('It was done successfully', {
+          type: 'success',
+          autoClose: 1000,
+        });
+      } else {
+        toast('Encountered an error', {type: 'error', autoClose: 1000});
       }
-    },
-  });
 
-  const saveAdmin = () => {
-    const viewerWithoutAdmin =
-      viewers?.data?.users
-        .filter(viewer => viewer.access !== AccessEnum.admin)
-        .map(viewer => ({
-          user_id: viewer.user.id,
-          access_types: AccessEnum.viewer,
-        })) || [];
-    const admin = viewers?.data?.users.find(
-      viewer => viewer.access === AccessEnum.admin,
-    );
-    viewerWithoutAdmin.push({
-      user_id: userAdmin || admin!.user.id,
-      access_types: AccessEnum.admin,
-    });
-    request('stationAddadmin', {
-      params: {station_id: params.stationId!},
-      data: {user_id: userAdmin || admin!.user.id!},
-    });
-    request('stationAccessUpdate', {
-      params: {station_id: params.stationId!},
-      data: {users: network?.stationviewers},
-    });
+    } catch (error) {
+      console.log(`error is:${error}`);
+      
+    } finally {
+      setLoading(false)
+    }
+
+    // request('stationAddadmin', {
+    //   params: {station_id: params.stationId!},
+    //   data: {user_id: userAdmin || admin!.user.id!},
+    // });
+    // request('stationAccessUpdate', {
+    //   params: {station_id: params.stationId!},
+    //   data: {users: network?.stationviewers},
+    // });
     dispatch(setstationviewers([]));
     dispatch(setstationviewersstatus(false));
   };
@@ -190,6 +257,7 @@ const StationAccessPage = () => {
                 Link Viewer(s)
               </span>
               <Table
+              loading={loading}
                 dynamicColumns={['index']}
                 renderDynamicColumn={data => data.index + 1}
                 tabicon={tabname}
@@ -209,7 +277,7 @@ const StationAccessPage = () => {
             </SimpleBtn>
 
             <SimpleBtn
-              onClick={network.stationviewersstatus ? saveAdmin : () => {}}>
+              onClick={network.stationviewersstatus ? saveAdmin : changeAdmin}>
               Save
             </SimpleBtn>
 
