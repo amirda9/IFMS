21f7a76cce64f453205b08f4ff8750fc9d99de9c
Behnam maghsoudi <behnam.maghsoudi17@gmail.com>
2025-02-05 11:21:06 +0330

    fix<opticalroutslice>I put the types in a separate file fix<opticalrouteType>add new types

--- src/store/slices/opticalroutslice.ts
+++ src/store/slices/opticalroutslice.ts
@@ -1,136 +1,115 @@
 import {createSlice} from '@reduxjs/toolkit';
-import {opticalrouteUpdateTestSetupDetailtype} from './../../types/opticalrouteType'
-import { deepcopy } from '~/util';
-import { number } from 'yup';
+import {opticalrouteUpdateTestSetupDetailtype} from './../../types/opticalrouteType';
+import {deepcopy} from '~/util';
+import {
+  veiwerlists,
+  networkopticalroutetype,
+  networkselectedlisttype,
+  networkopticaltypeAction,
+  alldeleteopticalroutetype,
+  alldeleteopticalroutetypeAction,
+} from '~/types/opticalrouteType';
 
-type veiwerlists = {
-  payload: opticalrouteUpdateTestSetupDetailtype;
-  type: string;
+type initialStatetype = {
+  opticalroutUpdateTestsetupDetail: opticalrouteUpdateTestSetupDetailtype;
+  networkselectedlist: string[];
+  networkoptical: networkopticalroutetype[];
+  alldeleteopticalroute: alldeleteopticalroutetype;
+  opticalroutenetworkidadmin: string[];
+  gettestsetupdetaildata: boolean;
+  modalloading: boolean;
+  openall: boolean;
 };
-export type networkopticaltype={networkid: string; opticalrouts: {name: string; id: string}[]}[]
-type networkselectedlisttype={
-  payload: string[];
-  type: string;
-}
-
-type networkopticaltypeAction={
-  payload:networkopticaltype;
-  type: string;
-}
-export  type alldeleteopticalroutetype={
-  networkid: string; opticalrouts:string[];
-}[]
-
-
-type alldeleteopticalroutetypeAction={
-  payload:alldeleteopticalroutetype;
-  type: string;
-}
-
-type networkopticalroutetype={networkid: string; opticalrouts: {name: string; id: string}[]}
-
-
-type initialStatetype={
-  opticalroutUpdateTestsetupDetail:opticalrouteUpdateTestSetupDetailtype
-  networkselectedlist:string[]
-  networkoptical:networkopticalroutetype[]
-  alldeleteopticalroute:alldeleteopticalroutetype
-  opticalroutenetworkidadmin:string[]
-  gettestsetupdetaildata:boolean,
-  modalloading:boolean,
-  openall:boolean
-}
-const initialState:initialStatetype = {
-  opticalroutUpdateTestsetupDetail:{    name: "",
-    station_id: "",
-    station_name: "",
-    init_rtu_id: "",
-    init_rtu_name: "",
-    startdatePart:"2023-11-28",
-    starttimePart:"22:26:03",
-    enddatePart:"2023-11-28",
-   endtimePart:"22:26:02",
+const initialState: initialStatetype = {
+  opticalroutUpdateTestsetupDetail: {
+    name: '',
+    station_id: '',
+    station_name: '',
+    init_rtu_id: '',
+    init_rtu_name: '',
+    startdatePart: '2023-11-28',
+    starttimePart: '22:26:03',
+    enddatePart: '2023-11-28',
+    endtimePart: '22:26:02',
     parameters: {
       enabled: true,
-      type: "Monitoring",
-      wavelength: "1625",
-      break_strategy: "Skip",
-      date_save_policy: "Save Trace File",
-      test_mode: "fast",
-      run_mode:"average",
-      distance_mode: "manual",
+      type: 'Monitoring',
+      wavelength: '1625',
+      break_strategy: 'Skip',
+      date_save_policy: 'Save Trace File',
+      test_mode: 'fast',
+      run_mode: 'average',
+      distance_mode: 'manual',
       range: 3,
-      pulse_width_mode: "manual",
+      pulse_width_mode: 'manual',
       pulse_width: 3,
-      sampling_mode: "duration",
+      sampling_mode: 'duration',
       sampling_duration: 4,
-      IOR:1.476,
+      IOR: 1.476,
       RBS: -79,
       event_loss_threshold: 0.05,
       event_reflection_threshold: -40,
       fiber_end_threshold: 5,
       total_loss_threshold: 5,
       section_loss_threshold: 5,
-      injection_level_threshold: 5
+      injection_level_threshold: 5,
     },
     learning_data: {
       targeted_count_per_cycle: 30,
       start_cycle_time: {
-        type: "fixed",
-        time: "2023-11-28 22:26:03",
+        type: 'fixed',
+        time: '2023-11-28 22:26:03',
         periodic_options: {
           value: 1,
-          period_time: "monthly"
-        }
+          period_time: 'monthly',
+        },
       },
       increase_count_options: {
         count: 2,
         timing: {
-          type: "fixed",
-          time: "2023-11-28 22:26:03",
+          type: 'fixed',
+          time: '2023-11-28 22:26:03',
           periodic_options: {
             value: 1,
-            period_time: "monthly"
-          }
+            period_time: 'monthly',
+          },
         },
-        maximum_count: 60
-      }
+        maximum_count: 60,
+      },
     },
     test_program: {
       starting_date: {
-        start:"2023-11-28 22:26:03",
-        immediately: false
+        start: '2023-11-28 22:26:03',
+        immediately: false,
       },
       end_date: {
-        end:"2023-11-28 22:26:02",
-        indefinite: false
+        end: '2023-11-28 22:26:02',
+        indefinite: false,
       },
       period_time: {
         value: 1,
-        period_time: "monthly"
-      }
+        period_time: 'monthly',
+      },
     },
     status: {
       reference_status: '',
-    current_learning_cycle: 1,
-    on_learning: false,
-    current_reference_id: '',
-    current_cycle_start: '',
-    next_cycle_start: '',
-    first_reference_time: '',
-    last_reference_time: '',
-    last_learning_count: 0
-  }
-    
-  
+      current_learning_cycle: 1,
+      on_learning: false,
+      current_reference_id: '',
+      current_cycle_start: '',
+      next_cycle_start: '',
+      first_reference_time: '',
+      last_reference_time: '',
+      last_learning_count: 0,
+    },
   },
-    networkselectedlist:[],
-    networkoptical:[],
-    alldeleteopticalroute:[],
-    opticalroutenetworkidadmin:[],
-    gettestsetupdetaildata:false,
-    modalloading:false,
-    openall:false
+  networkselectedlist: [],
+  networkoptical: [],
+  alldeleteopticalroute: [],
+  opticalroutenetworkidadmin: [],
+  gettestsetupdetaildata: false,
+  modalloading: false,
+  openall: false,
 };
 
 const opticalroute = createSlice({
@@ -146,10 +125,16 @@ const opticalroute = createSlice({
     setNetworkoptical: (state, action: networkopticaltypeAction) => {
       state.networkoptical = action.payload;
     },
-    setAlldeleteopticalroute: (state, action: alldeleteopticalroutetypeAction) => {
+    setAlldeleteopticalroute: (
+      state,
+      action: alldeleteopticalroutetypeAction,
+    ) => {
       state.alldeleteopticalroute = action.payload;
     },
-    setOpticalrouteNetworkidadmin: (state, action: {type: string; payload: string}) => {
+    setOpticalrouteNetworkidadmin: (
+      state,
+      action: {type: string; payload: string},
+    ) => {
       const findinlist = state.opticalroutenetworkidadmin.findIndex(
         data => data == action.payload,
       );
@@ -157,29 +142,40 @@ const opticalroute = createSlice({
         state.opticalroutenetworkidadmin.push(action.payload);
       }
     },
-    changeOpticalroutename: (state, action: {type: string; payload:{networkid:string,opticalId:string,opticalName:string}}) => {
-      console.log("🚞",action.payload);
-      
-      const networkopticalCopy:networkopticalroutetype[]=deepcopy(state.networkoptical)
+    changeOpticalroutename: (
+      state,
+      action: {
+        type: string;
+        payload: {networkid: string; opticalId: string; opticalName: string};
+      },
+    ) => {
+      const networkopticalCopy: networkopticalroutetype[] = deepcopy(
+        state.networkoptical,
+      );
       const findinlist = networkopticalCopy.findIndex(
         data => data.networkid == action.payload.networkid,
       );
-      console.log("🧑‍✈️",findinlist);
-      
-      const findoptical=networkopticalCopy[findinlist].opticalrouts.findIndex(data => data.id == action.payload.opticalId)
-      console.log("7🧑‍✈️7",findinlist);
-      networkopticalCopy[findinlist].opticalrouts[findoptical].name=action.payload.opticalName
- state.networkoptical=networkopticalCopy
+
+      const findoptical = networkopticalCopy[findinlist].opticalrouts.findIndex(
+        data => data.id == action.payload.opticalId,
+      );
+
+      networkopticalCopy[findinlist].opticalrouts[findoptical].name =
+        action.payload.opticalName;
+      state.networkoptical = networkopticalCopy;
     },
-    setgettestsetupdetaildata:(state, action: {type: string; payload: boolean}) => {
-      state.gettestsetupdetaildata=action.payload
+    setgettestsetupdetaildata: (
+      state,
+      action: {type: string; payload: boolean},
+    ) => {
+      state.gettestsetupdetaildata = action.payload;
     },
-    setmodalloading:(state, action: {type: string; payload: boolean}) => {
-      state.modalloading=action.payload
+    setmodalloading: (state, action: {type: string; payload: boolean}) => {
+      state.modalloading = action.payload;
     },
-    setopenall:(state, action: {type: string; payload: boolean}) => {
-      state.openall=action.payload
-    }
+    setopenall: (state, action: {type: string; payload: boolean}) => {
+      state.openall = action.payload;
+    },
   },
 });
 
@@ -192,7 +188,7 @@ export const {
   changeOpticalroutename,
   setgettestsetupdetaildata,
   setmodalloading,
-  setopenall
+  setopenall,
 } = opticalroute.actions;
 
 export default opticalroute.reducer;
--- src/types/opticalrouteType.ts
+++ src/types/opticalrouteType.ts
@@ -434,3 +434,33 @@ export type opticalrouteUpdateType = {
   time_created: string;
   time_updated: string;
 };
+
+
+
+export type veiwerlists = {
+  payload: opticalrouteUpdateTestSetupDetailtype;
+  type: string;
+};
+export type networkopticaltype={networkid: string; opticalrouts: {name: string; id: string}[]}[]
+export type networkselectedlisttype={
+  payload: string[];
+  type: string;
+}
+
+export type networkopticaltypeAction={
+  payload:networkopticaltype;
+  type: string;
+}
+export  type alldeleteopticalroutetype={
+  networkid: string; opticalrouts:string[];
+}[]
+
+
+export type alldeleteopticalroutetypeAction={
+  payload:alldeleteopticalroutetype;
+  type: string;
+}
+
+export type networkopticalroutetype={networkid: string; opticalrouts: {name: string; id: string}[]}
+
+
